9dd1b2ef4c3e566a949d72dcaa585e249abf7da3
agnesdonat <agi.donat@gmail.com>
2018-03-08 15:51:37 +0000

    Adding players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,12 @@
+require 'capybara/rspec'
+
+
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player1_name, with: 'Agnes'
+    fill_in :player2_name, with: 'Reena'
+    click_button 'Submit'
+    expect(page).to have_content 'Agnes vs Reena'
   end
 end
